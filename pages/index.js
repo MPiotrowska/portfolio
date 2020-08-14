5c9c088f9980afc639aa488f10c217fe41a92277
MPiotrowska <monikapiotrowska27@gmail.com>
2020-08-14 20:02:18 +0200

    fetch starred repos

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import React from "react";
+import Head from "next/head";
+import Layout from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Repo from "../components/Repo";
 
 export default function Home() {
+  const [repos, setRepos] = React.useState([]);
+
+  React.useEffect(() => {
+    const url = "https://api.github.com/users/MPiotrowska/starred?per_page=10";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setRepos(data);
+      });
+  }, []);
+
   return (
     <Layout>
       <Head>
@@ -11,37 +24,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <section
+        style={{ maxWidth: "36rem", margin: "auto" }}
+        className={utilStyles.headingMd}
+      >
+        <p>Hi, I'm Monika!</p>
+        <p>I love building stuff!</p>
+      </section>
+
       <main>
-    
-
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {repos.length > 0 &&
+            repos.map((repo) => {
+              return <Repo name={repo.name} />;
+            })}
         </div>
       </main>
 
@@ -51,7 +47,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/images/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -145,36 +141,6 @@ export default function Home() {
           margin-top: 3rem;
         }
 
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
         .logo {
           height: 1em;
         }
@@ -202,5 +168,5 @@ export default function Home() {
         }
       `}</style>
     </Layout>
-  )
+  );
 }
