c03da3ce8daf8493e04851e72ed33ea4f64ca7ec
MPiotrowska <monikapiotrowska27@gmail.com>
2020-08-15 15:48:27 +0200

    refactor layout

--- components/layout.js
+++ components/layout.js
@@ -2,65 +2,73 @@ import Head from "next/head";
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
+import content from "../content.json";
 
-const name = "Monika Piotrowska";
 export const siteTitle = "Next.js Sample Website";
+const name = "Monika Piotrowska";
 
-export default function Layout({ children, home }) {
+export default function Layout({ children }) {
   return (
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.now.sh/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+        <meta name="description" content="Monika Piotrowska | Web Developer" />
+        <meta property="og:image" content="/images/profile.jpeg" />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
-        {home ? (
+        <article className={styles.column}>
+          <Link href="/">
+            <a>{name}</a>
+          </Link>
+        </article>
+        <article className={styles.column}>
+          <nav className={styles.innerColumn}>
+            <ul className={styles.socialLinks}>
+              <li>
+                <Link href={content.navigation.github}>
+                  <a>Github</a>
+                </Link>
+              </li>
+              <li>
+                <Link href={content.navigation.linkedin}>
+                  <a>Linkedin</a>
+                </Link>
+              </li>
+              <li>
+                <Link href={content.navigation.twitter}>
+                  <a>Twitter</a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </article>
+      </header>
+      <section className={styles.section}>
+        {
           <>
-            <img
-              src="/images/profile.jpeg"
-              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
+            
             <Link href="/">
               <a>
                 <img
                   src="/images/profile.jpeg"
-                  className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                  className={`${styles.sectionImage} ${utilStyles.borderCircle}`}
                   alt={name}
                 />
               </a>
             </Link>
             <h2 className={utilStyles.headingLg}>
               <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
+                <a className={utilStyles.colorInherit}>
+                  {content.intro.heading}
+                </a>
               </Link>
             </h2>
           </>
-        )}
-      </header>
+        }
+      </section>
       <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
     </div>
   );
 }
